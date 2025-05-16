9168851bc5e942b6f79ded3aa275eaa4b447cc85
dishant-08 <dishant.sahu07@gmail.com>
2025-05-16 17:00:18 +0530

    fix

--- internal/protocols/obdii2g/obdii2g.go
+++ internal/protocols/obdii2g/obdii2g.go
@@ -41,7 +41,7 @@ func (a *AquilaOBDII2GProtocol) GetProtocolType() types.DeviceProtocolType {
 }
 
 func (a *AquilaOBDII2GProtocol) Login(reader *bufio.Reader) ([]byte, int, error) {
-	peeked, err := reader.Peek(22)
+	peeked, err := reader.Peek(32)
 	if err != nil {
 		// return nil, 0, errors.Wrap(err, "failed to peek login packet")
 	}
@@ -64,9 +64,9 @@ func (a *AquilaOBDII2GProtocol) Login(reader *bufio.Reader) ([]byte, int, error)
 	}
 
 	parts := strings.Split(packetStr, ",")
-	if len(parts) < 3 {
-		// return nil, 0, errors.New("invalid login packet format")
-	}
+	// if len(parts) < 3 {
+	// return nil, 0, errors.New("invalid login packet format")
+	// }
 
 	// Extract IMEI from second field (index 1)
 	imei := parts[1]
