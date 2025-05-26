63bed57ecb4a69c5804388b3dfe975af911e34db
dishant-08 <dishant.sahu07@gmail.com>
2025-05-26 13:41:48 +0530

    added log

--- internal/protocols/obdii2g/obdii2g.go
+++ internal/protocols/obdii2g/obdii2g.go
@@ -99,6 +99,7 @@ func (a *AquilaOBDII2GProtocol) ConsumeStream(reader *bufio.Reader, writer io.Wr
 			}
 		default:
 			packet, err := reader.ReadString('*')
+			logger.Sugar().Infoln("Full Packet", packet)
 			if err != nil {
 				if errors.Is(err, io.EOF) {
 					logger.Info("Connection closed gracefully")
